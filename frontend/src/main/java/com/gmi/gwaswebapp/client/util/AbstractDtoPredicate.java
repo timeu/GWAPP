989f0d54deb9283ecf5075ffc4c1a983eb720d47
uemit.seren <uemit.seren@gmi.oeaw.ac.at>
2011-12-20 17:11:19 +0100

    TYPE added

--- frontend/src/main/java/com/gmi/gwaswebapp/client/util/AbstractDtoPredicate.java
+++ frontend/src/main/java/com/gmi/gwaswebapp/client/util/AbstractDtoPredicate.java
@@ -2,13 +2,33 @@ package com.gmi.gwaswebapp.client.util;
 
 public abstract class AbstractDtoPredicate<T,S> implements Predicate<T> {
 	
+	public enum TYPE {AND,OR} 
 	protected S value;
+	protected TYPE type;
+	
 	
 	public AbstractDtoPredicate(S value) {
+		this(value,TYPE.AND);
+	}
+	
+	public AbstractDtoPredicate(S value, TYPE type) {
 		this.value = value;
+		this.type = type;
 	}
 	
 	public void setValue(S value) {
 		this.value = value;
+	}
+	
+	public S getvalue() {
+		return value;
+	}
+	
+	public void setType(TYPE type) {
+		this.type = type;
+	}
+	
+	public TYPE getType() {
+		return type;
 	}
 }
